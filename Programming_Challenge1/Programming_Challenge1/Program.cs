6c59c7c18627c06e9d7ccb2cf05e9ed872340694
Alanduril <66563565+Alanduril@users.noreply.github.com>
2025-07-04 22:10:55 +0200

    First Challenge
    First Challenge

---
+++ Programming_Challenge1/Programming_Challenge1/Program.cs
@@ -0,0 +1,23 @@
+﻿string userInput = "a";
+
+Console.WriteLine("Please input any word:");
+userInput = Console.ReadLine();
+userInput = UppercaseConverter(userInput);
+ArrayWritter(userInput);
+Console.ReadKey();
+
+static string UppercaseConverter (string input)
+    {
+        input = input.ToUpper();
+        return input;
+    }
+static string ArrayWritter(string input)
+{
+    input.ToCharArray();
+
+    foreach (char c in input)
+    {
+        Console.WriteLine(c);        
+    }
+    return null;
+}
